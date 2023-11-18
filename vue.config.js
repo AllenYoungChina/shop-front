09b3561fe6689accd179bec0797acbcbadb6431a
AYC <allenyoungchina@outlook.com>
2023-11-18 17:25:30 +0800

    前端解决跨域问题

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,32 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+// 解决跨域问题
+const path = require('path')
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  productionSourceMap: false,
+  devServer: {
+    hot: true,
+    port: 8010,
+    open: true,
+    proxy: {
+      '/': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        pathRewrite: {'^/': ''}
+      }
+    }
+  },
+  configureWebpack: {
+    name: 'system',
+    resolve: {
+      alias: {
+        '~@': __dirname,
+        '@': path.resolve(__dirname, './src')
+      }
+    }
+  }
+}
