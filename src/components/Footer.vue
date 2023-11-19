6e062c00c9c7edea42513fd2b471706fb421bf45
AYC <allenyoungchina@outlook.com>
2023-11-19 15:10:59 +0800

    新增公共组件导航和页脚

---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="footer">
+    <div class="ng-promise-box">
+      <div class="ng-promise w1200">
+        <p class="text">
+          <a href="javascript:;" class="icon1">
+            7天无理由退换货
+          </a>
+          <a href="javascript:;" class="icon2">
+            满99元全场包邮
+          </a>
+          <a href="javascript:;" class="icon3" style="margin-right: 0">
+            100%品质保证
+          </a>
+        </p>
+      </div>
+    </div>
+    <div class="mod_help w1200">
+      <p>
+        <a href="javascript:;">关于我们</a>
+        <span>|</span>
+        <a href="javascript:;">帮助中心</a>
+        <span>|</span>
+        <a href="javascript:;">售后服务</a>
+        <span>|</span>
+        <a href="javascript:;">母婴资讯</a>
+        <span>|</span>
+        <a href="javascript:;">关于货源</a>
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
